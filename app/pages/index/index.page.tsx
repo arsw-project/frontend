e0ffeb2c0111c590c417565af85abecad1d291ae
T-Hash06 <tomasfelipe.p@gmail.com>
2025-09-18 15:54:13 -0500

    chore: initial commit

---
+++ app/pages/index/index.page.tsx
@@ -0,0 +1,14 @@
+export function meta() {
+	return [
+		{ title: 'New React Router App' },
+		{ name: 'description', content: 'Welcome to React Router!' },
+	];
+}
+
+export default function Home() {
+	return (
+		<main className="w-dvw h-dvh grid place-items-center">
+			<h1 className="text-4xl font-bold">ARSW Project - Frontend</h1>
+		</main>
+	);
+}
